7eeb45ab62cef89ea5e036f0df941f5d51829f75
Kyle Harris <kaharrs2@gmail.com>
2024-12-05 08:27:10 -0700

    day5 passes test not input

---
+++ solve5.py
@@ -0,0 +1,52 @@
+# Load puzzle
+rules_left = {}
+rules_right = {}
+updates = []
+
+def check_order(update, rules_left, rules_right):
+    for i, current in enumerate(update):
+        try:
+            other_right = rules_left[current]
+            try:
+                if i > update.index(other_right):
+                    return False
+            except:
+                pass
+        except KeyError:
+            pass
+
+        try:
+            other_left = rules_right[current]
+            try:
+                if i < update.index(other_left):
+                    return False
+            except:
+                pass
+        except KeyError:
+            pass
+
+    return True
+
+with open("05.txt", "r") as fid:
+    nextpart = False
+    print("part 1")
+    for y, line in enumerate(fid):
+        if line == "\n":
+            nextpart = True
+        else:
+            if not nextpart:
+                # Read first part
+                s = line.split('|')
+                rules_left[int(s[0])] = int(s[1])
+                rules_right[int(s[1])] = int(s[0])
+            else:
+                # Now read second part
+                updates.append([int(x) for x in line.split(',')])
+
+    accum = 0
+    for update in updates:
+        if check_order(update, rules_left, rules_right):
+            print(update)
+            middle = len(update) // 2
+            accum += update[middle]
+    print(accum)
