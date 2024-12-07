d45771f5ebbd3a11e60ddc50ac301d7765866c58
Kyle Harris <kaharrs2@gmail.com>
2024-12-07 14:00:21 -0700

    close

--- include/06.h
+++ include/06.h
@@ -3,12 +3,14 @@
 #include <cstdint>
 #include <exception>
 #include <fstream>
+#include <functional>
 #include <iomanip>
 #include <iostream>
 #include <iterator>
 #include <map>
 #include <sstream>
 #include <string>
+#include <type_traits>
 #include <utility>
 #include <vector>
 
@@ -58,7 +60,7 @@ template <typename T>
 class Map2D {
 public:
     template <typename F>
-    Map2D(std::vector<std::string> lines, F map_char_to_T) {
+    Map2D(std::vector<std::string> lines, F&& map_char_to_T) {
         for (int64_t y = 0; y < lines.size(); y++) {
             for (int64_t x = 0; x < lines[y].size(); x++) {
                 m_map.insert({std::make_pair(x, y), map_char_to_T(lines[y][x])});
@@ -69,10 +71,37 @@ public:
     std::map<std::pair<int64_t, int64_t>, T> m_map = {};
 };
 
+std::map<std::pair<int64_t, int64_t>, char> make_map(std::vector<std::string> lines)
+{
+    std::map<std::pair<int64_t, int64_t>, char> output = {};
+    for (int64_t y = 0; y < lines.size(); y++) {
+        for (int64_t x = 0; x < lines[y].size(); x++) {
+            output.insert({std::make_pair(x, y), lines[y][x]});
+        }
+    }
+
+    return output;
+};
+
+template <typename F>
+std::map<std::pair<int64_t, int64_t>, auto> make_map(std::vector<std::string> lines, F&& map_char_to_T)
+{
+    std::map<std::pair<int64_t, int64_t>, decltype(map_char_to_T(std::declval<char>()))> output = {};
+    for (int64_t y = 0; y < lines.size(); y++) {
+        for (int64_t x = 0; x < lines[y].size(); x++) {
+            output.insert({std::make_pair(x, y), map_char_to_T(lines[y][x])});
+        }
+    }
+
+    return output;
+};
+
 int solve06() {
     //auto input = load(6);
     auto lines = load("06a.txt");
-    auto map = Map2D(lines, [](char c) -> char { return c; });
+    auto map1 = Map2D<char>(lines, [](char c) { return c; });
+    auto map2 = make_map(lines);
+    auto map3 = make_map(lines, [](char c) -> char { return c; });
 
     return 0;
 }
