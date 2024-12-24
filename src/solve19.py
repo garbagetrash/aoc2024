ae16f6257a993f15ab1af8f90e68cf0a1d2d1e20
garbagetrash <43052613+garbagetrash@users.noreply.github.com>
2024-12-24 13:32:26 -0700

    19p2

--- src/solve19.py
+++ src/solve19.py
@@ -1,44 +1,52 @@
 # Global pattern cache, contains achievable patterns
 cache = {}
+towels = []
 
-def pattern_solver(pattern, layer=0):
+def pattern_solver(pattern):
+    #print(f"Pattern: {pattern}")
     try:
         return cache[pattern]
     except:
-        pass
-    for split_point in range(1, len(pattern)):
-        #print(f"splitting at index: {split_point}")
-        if pattern_solver(pattern[:split_point], layer=layer+1):
-            # only bother with right if left was solvable.
-            if pattern_solver(pattern[split_point:], layer=layer+1):
-                # left and right solvable so pattern can be constructed
-                cache[pattern] = True
-                return True
-    cache[pattern] = False
-    return False
+        # Add new pattern to cache if not already there
+        cache[pattern] = 0
+
+    nways = 0
+    for t in towels:
+        if t == pattern[:len(t)]:
+            # Matches head
+            #print(f"{t} matches head of {pattern}")
+            if len(t) == len(pattern):
+                nways += 1
+            else:
+                nways += pattern_solver(pattern[len(t):])
+
+    # Update cache solution
+    #print(f"{pattern} can be made {nways} ways")
+    cache[pattern] = nways
+    return nways
 
 with open("../inputs/19.txt", "r") as fid:
     coords = []
     maxval = 0
-    pattern_list = []
     patterns = []
     for i, line in enumerate(fid):
         if i == 0:
             pattern_list = line.strip().split(', ')
             for p in pattern_list:
-                cache[p] = True
+                towels.append(p)
         elif i > 1:
             patterns.append(line.strip())
-    
-    print(patterns)
 
     # Part 1
     accum = 0
     for p in patterns:
-        if pattern_solver(p):
-            print(f"Desired: {p} has solution")
+        if pattern_solver(p) > 0:
             accum += 1
     print(accum)
 
     # Part 2
-    print(0)
+    accum = 0
+    for p in patterns:
+        nsolves = pattern_solver(p)
+        accum += nsolves
+    print(accum)
