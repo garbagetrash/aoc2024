7bf1f3dec18dcc7dad83f1b7df3d13ebbac893aa
garbagetrash <43052613+garbagetrash@users.noreply.github.com>
2024-12-24 11:30:35 -0700

    Update solve18.py

--- src/solve18.py
+++ src/solve18.py
@@ -1,3 +1,14 @@
+def build_map(coords, maxval, n):
+    m = {}
+    for x in range(maxval + 1):
+        for y in range(maxval + 1):
+            if (x, y) in coords[:n]:
+                m[(x, y)] = 1
+            else:
+                m[(x, y)] = 0
+    return m
+
+
 def neighbors(p, m):
     c = [(p[0], p[1] + 1), (p[0], p[1] - 1), (p[0] + 1, p[1]), (p[0] - 1, p[1])]
     output = []
@@ -13,9 +24,9 @@ def neighbors(p, m):
 def shortest_path(p1, p2, m):
     frontier = [(p1, 0)]
     explored = set()
-    while True:
+    while len(frontier) > 0:
         for n in neighbors(frontier[0][0], m):
-            print(n)
+            #print(n)
             fpoints = [f[0] for f in frontier]
             if n not in explored and n not in fpoints:
                 if n == p2:
@@ -23,6 +34,9 @@ def shortest_path(p1, p2, m):
                 frontier.append((n, frontier[0][1] + 1))
         explored.add(frontier[0][0])
         del frontier[0]
+
+    # No path
+    return None
 
 
 with open("../inputs/18.txt", "r") as fid:
@@ -38,18 +52,14 @@ with open("../inputs/18.txt", "r") as fid:
             maxval = y
         coords.append((int(x), int(y)))
 
-    print(coords)
-    m = {}
-    for x in range(maxval + 1):
-        for y in range(maxval + 1):
-            if (x, y) in coords[:1024]:
-                m[(x, y)] = 1
-            else:
-                m[(x, y)] = 0
+    m = build_map(coords, maxval, 1024)
 
     # Part 1
     print(shortest_path((0, 0), (maxval, maxval), m))
 
     # Part 2
-    accum = 0
-    print(accum)
+    for n in range(1020, len(coords)):
+        m[coords[n]] = 1
+        if shortest_path((0, 0), (maxval, maxval), m) is None:
+            print(f"{coords[n][0]},{coords[n][1]}")
+            break
